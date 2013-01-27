10ccd8c94efeba5cc0f768b73dc18a901c823e33
Alex Ehlke <alex.ehlke@gmail.com>
2013-01-27 18:07:19 -0500

    use CACHE_KEY_PREFIX instead of KEY_PREFIX for Django<1.3

--- cachecow/cache.py
+++ cachecow/cache.py
@@ -113,8 +113,9 @@ def make_key(obj, namespace=None):
         namespace = make_key(namespace)
         key = '{}:{}'.format(_get_namespace_prefix(namespace), key)
 
-    # Backwards-compatibility for Django < 1.3.
-    if not hasattr(cache, 'key_prefix') and getattr(settings, 'KEY_PREFIX', None):
+    # Use cache.key_prefix if available (Django>=1.3), otherwise CACHE_KEY_PREFIX.
+    if (not hasattr(cache, 'key_prefix')
+            and getattr(settings, 'CACHE_KEY_PREFIX', None)):
         key = '{}:{}'.format(settings.KEY_PREFIX, key)
 
     try:
